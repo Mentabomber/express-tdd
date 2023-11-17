9120db9d2365aa9ed3b07d7302465e1fe8d96448
Mentabomber <mentae_mentology@libero.it>
2023-11-17 09:29:36 +0100

    task 4

--- test/createSlug.js
+++ test/createSlug.js
@@ -1,9 +1,53 @@
-module.exports =  function (str){
-    if (typeof str === "string") {
-        let slug = str;
-        return slug;
+const slugs = [
+    
+    {
+        "title": "slug mario",
+        "slug": "slug-mario"          
+    },
+    {
+        "title": "slug mario 1",
+        "slug": "slug-mario-1"          
+    },
+    {
+        "title": "slug marione",
+        "slug": "slug-marione"          
     }
-    else{
+
+];
+
+
+module.exports =  function (str){
+
+    if (typeof str !== "string") {
         throw new Error("input must be a string");
     }
+        
+    let slug = str;
+
+    let i = 1;
+
+    do {
+        // Generate a new slug by replacing spaces with "-", adding the counter, and converting to lowercase
+        const newSlug = slug.replace(/ /g, "-") + (i > 1 ? `-${i}` : "");
+
+        // Check if the newly generated slug or modified slug with the counter already exists in the slugs array
+        const slugExists = slugs.some(existingSlug => existingSlug.slug === slug || existingSlug.slug === newSlug);
+
+        // If the slug exists, increment the counter; otherwise, use the current counter value
+        i = slugExists ? i + 1 : i;
+
+        // Update the slug with the modified counter
+        slug = slug.replace(/ /g, "-") + (i > 1 ? `-${i}` : "");
+
+    } while (slugs.some(existingSlug => existingSlug.slug === slug));
+
+    return slug.toLowerCase();
+  
+    // slug.split("").map((char) => {
+    //     if (char === " "){
+    //         return "-";
+    //     }
+    // return char;
+    // }).join("").toLowerCase();
+
 }
--- test/createSlug.test.js
+++ test/createSlug.test.js
@@ -8,3 +8,24 @@ test("createSlug dovrebbe ritornare una stringa", () => {
 
     expect(typeof result).toBe("string");
 })
+
+test("createSlug dovrebbe ritornare un testo tutto in lowercase", () => {
+    const testo = "TeStO";
+    const result = createSlug(testo);
+
+    expect(result).toBe("testo");
+})
+
+test("createSlug dovrebbe ritornare una stringa con gli spazi sostituiti da -", () => {
+    const testo = "questo è un testo"
+    const result = createSlug(testo);
+
+    expect(result).toBe("questo-è-un-testo");
+})
+
+test("createSlug dovrebbe incrementare di 1 lo slug quando esiste già", () => {
+    const testo = "slug mario";
+    const result = createSlug(testo);
+
+    expect(result).toBe("slug-mario-2");
+})
